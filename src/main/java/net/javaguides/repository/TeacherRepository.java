45693fc5a5ade1f249700f25491b505dc56367d6
adesh dhope <146838880+Adesh-Dhope@users.noreply.github.com>
2025-12-31 09:01:15 +0530

    Change Teacher ID type from Long to String

--- src/main/java/net/javaguides/repository/TeacherRepository.java
+++ src/main/java/net/javaguides/repository/TeacherRepository.java
@@ -3,6 +3,6 @@ package net.javaguides.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import net.javaguides.model.Teacher;
 
-public interface TeacherRepository extends MongoRepository<Teacher, Long> {
+public interface TeacherRepository extends MongoRepository<Teacher, String> {
 }
 
